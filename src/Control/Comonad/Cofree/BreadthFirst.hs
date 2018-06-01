b03823e43a97e7a6d73c16f2e5356d3b66e52cb6
Donnacha Oisín Kidney <mail@doisinkidney.com>
2018-06-01 11:21:33 -0400

    cleaned up again

--- src/Control/Comonad/Cofree/BreadthFirst.hs
+++ src/Control/Comonad/Cofree/BreadthFirst.hs
@@ -1,14 +1,17 @@
 {-# LANGUAGE LambdaCase #-}
 
-module Control.Comonad.Cofree.BreadthFirst where
+module Control.Comonad.Cofree.BreadthFirst
+  (breadthFirst
+  ,ibreadthFirst)
+  where
 
 import           Control.Comonad.Cofree
 
 import           Control.Applicative
 import           Control.Monad.State.Simple
+-- import           Control.Applicative.Backwards
 -- import           Data.Bifunctor
 -- import           Data.Functor.Compose
--- import           Data.Monoid                (Endo (..))
 -- import           Data.Profunctor.Unsafe
 
 -- breadthFirst
@@ -20,15 +23,24 @@ import           Control.Monad.State.Simple
 --         (map2 (:<) (c t) fill)
 --         (foldr (liftA2 evalState) (pure []) chld)
 --   where
---     (chld,fill) = go [] ts
---     go b =
---         first (`appEndo` b) .
---         getCompose #.
---         traverse (\x -> Compose (Endo (f x), State (\(y:ys) -> (y, ys))))
---     f (x:<xs) (q:qs) = app2 (\y ys zs -> (y:<ys) : zs) (c x) r q : rs where (rs,r) = go qs xs
---     f (x:<xs) []     = map2 (\y ys    -> [y:<ys]     ) (c x) r   : rs where (rs,r) = go [] xs
---     map2 = flip . (fmap .) . flip . (fmap .)
+--     (fill,chld) = levl ts []
+
+--     levl = (runState . forwards . getCompose)
+--         #. traverse
+--                ((Compose . Backwards . State . (first State .))
+--             #. (((,) uncons .) . f))
+
+--     f (x:<xs) (q:qs) = app2 (\y ys zs -> (y:<ys) : zs) (c x) r q : rs where (r,rs) = levl xs qs
+--     f (x:<xs) []     = map2 (\y ys    -> [y:<ys]     ) (c x) r   : rs where (r,rs) = levl xs []
+
+--     map2 = flip . (fmap   .) . flip . (fmap   .)
 --     app2 = flip . (liftA2 .) . flip . (liftA2 .)
+
+uncons :: [a] -> (a, [a])
+uncons  = \case
+    (x:xs) -> (x,xs)
+    []     -> errorWithoutStackTrace "uncons: empty list!"
+{-# INLINE uncons #-}
 
 breadthFirst
     :: (Applicative f, Traversable t)
@@ -36,12 +48,10 @@ breadthFirst
 breadthFirst c (t:<ts) = liftA2 evalState (map2 (:<) (c t) (fill ts)) chld
   where
     chld = foldr (liftA2 evalState) (pure []) (foldr f [] ts)
+    fill = traverse (const (State uncons))
     {-# INLINE chld #-}
-    fill = traverse (const (State (\case
-                                        (x:xs) -> (x, xs)
-                                        [] -> errorWithoutStackTrace
-                                                  "Control.Comonad.BreadthFirst: bug!")))
     {-# INLINE fill #-}
+
     f (x:<xs) (q:qs) = app2 (\y ys zs -> (y:<ys) : zs) (c x) (fill xs) q : foldr f qs xs
     f (x:<xs) []     = map2 (\y ys    -> [y:<ys]     ) (c x) (fill xs)   : foldr f [] xs
 
@@ -59,10 +69,7 @@ ibreadthFirst c (t:<ts) = liftA2 evalState (map2 (:<) (c 0 t) (fill ts)) chld
   where
     chld = foldr (liftA2 evalState) (pure []) (foldr (f 1) [] ts)
     {-# INLINE chld #-}
-    fill = traverse (const (State (\case
-                                        (x:xs) -> (x, xs)
-                                        [] -> errorWithoutStackTrace
-                                                  "Control.Comonad.BreadthFirst: bug!")))
+    fill = traverse (const (State uncons))
     {-# INLINE fill #-}
     f i (x:<xs) (q:qs) = app2 (\y ys zs -> (y:<ys) : zs) (c i x) (fill xs) q : foldr (f (i+1)) qs xs
     f i (x:<xs) []     = map2 (\y ys    -> [y:<ys]     ) (c i x) (fill xs)   : foldr (f (i+1)) [] xs
