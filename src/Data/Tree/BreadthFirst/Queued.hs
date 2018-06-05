85f0a58964c4546293fee76d9dca5d9dd3476f14
Donnacha Oisín Kidney <mail@doisinkidney.com>
2018-06-05 13:22:44 -0400

    working one-pass queue

--- src/Data/Tree/BreadthFirst/Queued.hs
+++ src/Data/Tree/BreadthFirst/Queued.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Data.Tree.BreadthFirst.Queued
   (levels
   ,breadthFirst
@@ -5,8 +7,9 @@ module Data.Tree.BreadthFirst.Queued
   where
 
 import           Control.Applicative
+import           Control.Applicative.Backwards
 import           Control.Monad.State.Simple
-import           Data.Tree                     hiding (levels,unfoldForest)
+import           Data.Tree                     hiding (levels, unfoldForest)
 import           Data.Tree.BreadthFirst.Common
 
 levels :: Tree a -> [[a]]
@@ -33,12 +36,32 @@ breadthFirst c tr = fmap head (f b tr e [])
 unfold :: Monad m => (b -> m (a, [b])) -> b -> m (Tree a)
 unfold f b = f b >>= \(x,xs) -> fmap (Node x) (unfoldForest f xs)
 
-unfoldForest :: Monad m => (b -> m (a, [b])) -> [b] -> m (Forest a)
-unfoldForest f ts = b [ts] (const id)
-  where
-    b [] k = pure (k (pure []) [])
-    b qs k = foldl (foldr t) b qs [] (\x xs -> k (pure []) (evalState x xs))
+-- unfoldForest :: Monad m => (b -> m (a, [b])) -> [b] -> m (Forest a)
+-- unfoldForest f ts = b [ts] (const id)
+--   where
+--     b [] k = pure (k (pure []) [])
+--     b qs k = foldl (foldr t) b qs [] (\x xs -> k (pure []) (evalState x xs))
 
-    t a fw bw k = do
-        (x,cs) <- f a
-        fw (cs : bw) (k . liftA2 (:) (fmap (Node x) (fill cs)))
+--     t a fw bw k = do
+--         (x,cs) <- f a
+--         fw (cs : bw) (k . liftA2 (:) (fmap (Node x) (fill cs)))
+
+unfoldForest :: Monad m => (b -> m (a, [b])) -> [b] -> m (Forest a)
+unfoldForest f ts = b [ts] (\ls -> concat . foldr run id ls)
+  where
+    b [] k = pure (k [] [])
+    b qs k = foldl g b qs [] (\ls -> k [] . foldr run id ls)
+
+    g a xs qs k = foldr t (\ls ys -> a ys (k . (:) ls)) xs (pure id) qs
+
+    t a fw xs bw = f a >>= \(x,cs) -> fw (liftA2 (.) xs (fmap (:) (pop x))) (cs:bw)
+
+    run x xs ys = uncurry (:) $ runState (x <*> (State (\zs -> ([], xs zs))) ) ys
+        -- x <- x'
+        -- xs <- xs'
+        -- modify ((x xs):)
+        -- pure []
+
+    go st = uncurry (:) . runState st
+
+    pop x  = State (\(y:ys) -> (Node x y, ys))
